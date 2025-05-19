3bf525c26e684e93bc2fb6045b84eeb75ef01736
BeeingAStudentSucks <ilija.milovanovic@stud.hslu.ch>
2025-05-19 17:12:10 +0200

    MainActivity.kt added back

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.bachelorthesis.beekeeper"
         minSdk = 34
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
---
+++ app/src/main/java/com/bachelorthesis/beekeeper/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bachelorthesis.beekeeper
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+}
